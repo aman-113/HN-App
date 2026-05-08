5e399468d29fdd4d444aa5323e26585fc04f00d5
Mrkhan936 <mrkhan9368@gmail.com>
2026-05-08 17:52:33 +0530

    Update CORS configuration to correct Vercel app URL

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: [process.env.CLIENT_URL || 'http://localhost:3000', 'https://hn-app-pied.vercel.app'],
+  origin: [process.env.CLIENT_URL || 'http://localhost:3000', 'https://hn-app-ten.vercel.app/'],
   credentials: true,
 }));
 
